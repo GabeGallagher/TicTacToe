943db478c644f905cccc35926fe97e9a000876df
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-05-24 13:30:51 -0400

    Added game board

--- Assets/Scripts/TileControl.cs
+++ Assets/Scripts/TileControl.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileControl : MonoBehaviour {
+public class TileControl : MonoBehaviour
+{
+    public Color color;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = gameObject.transform.Find("Sprite").GetComponent<SpriteRenderer>();
+        if (sprite == null)
+        {
+            Debug.Log("Unable to reference " + gameObject.name + "'s sprite");
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        //change tile color
+        //Debug.Log("Moused over " + gameObject.name);
+        sprite.color = color;
+    }
+
+    private void OnMouseExit()
+    {
+        //Debug.Log("Mouse off " + gameObject.name);
+        sprite.color = Color.white;
+    }
 }
