ced8d33352e6c98e42448d6dc79ab706abab938d
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-05-25 10:34:02 -0400

    Added Start Menu functionality (start and quit)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
 }
