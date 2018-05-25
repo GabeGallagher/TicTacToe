596d4477af159335183df7c8918d168fc6895ab7
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-05-25 10:23:18 -0400

    Added Main Menu Scene

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    bool playerOneTurn;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
