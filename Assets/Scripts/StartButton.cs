81983d3c0b6e811988a95e682beefbf5d2cfbcd6
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-05-28 12:32:12 -0400

    Added functionality to select different board sizes

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public Text title;
+
+    bool isClickable = false;
+
+    private void OnMouseEnter()
+    {
+        isClickable = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isClickable = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (isClickable)
+        {
+            transform.parent.parent.Find("ButtonsParent_01").gameObject.SetActive(true);
+            title.text = "Select a Board Size to Start Your Game!";
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
