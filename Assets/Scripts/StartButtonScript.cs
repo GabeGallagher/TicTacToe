4296f19913ed4b62095423cb13ab68297e10e4e0
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-05-28 17:31:35 -0400

    Fixed issue where canvas elements weren't scaling properly with adjusted screen size

---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public Text title;
+
+    public GameObject buttonsParent, buttonsParent_01;
+
+    bool isClickable = false;
+
+    public void EnableNewButtons()
+    {
+        buttonsParent_01.SetActive(true);
+        title.text = "Select a Board Size to Start Your Game!";
+        buttonsParent.SetActive(false);
+    }
+}
